a0a8d1d23b3036aa7dd1dfef1562e708eb496237
Gabriel Davi <gabrielbaiamota765@gmail.com>
2026-06-30 19:22:33 -0300

    feat: add relacionamentos

--- sql/Create.sql
+++ sql/Create.sql
@@ -174,3 +174,186 @@ CREATE TABLE Animais(
     PRIMARY KEY (Nome, Codigo),
     CONSTRAINT fk_animais_personagem FOREIGN KEY (Codigo) REFERENCES Personagem(Codigo)
 );  
+
+
+
+-- Relações 
+
+CREATE TABLE Casou (
+    Codigo_Personagem1 INT,
+    Codigo_Personagem2 INT,
+    DataComeco INT,
+    DataFim INT,
+    Periodo INT,
+    MotivoFim VARCHAR(100),
+    
+    -- Chave primária composta para permitir que casem de novo se houver anulação
+    PRIMARY KEY (Codigo_Personagem1, Codigo_Personagem2, DataComeco),
+    CONSTRAINT fk_casou_p1 FOREIGN KEY (Codigo_Personagem1) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_casou_p2 FOREIGN KEY (Codigo_Personagem2) REFERENCES Personagem(CODIGO)
+);
+
+CREATE TABLE Afilia(
+    Codigo INT PRIMARY KEY,
+    Pai INT,
+    Mae INT,
+
+    CONSTRAINT fk_afilia_pai FOREIGN KEY (Pai) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_afilia_mae FOREIGN KEY (Mae) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_afilia_personagem FOREIGN KEY (Codigo) REFERENCES Personagem(CODIGO)
+);
+
+
+CREATE TABLE JaMontou(
+    Codigo_Personagem INT,
+    Nome_Dragao VARCHAR(50),
+    Data_Inicio INT,
+    Data_Fim INT,
+    
+    PRIMARY KEY (Codigo_Personagem, Nome_Dragao),
+    CONSTRAINT fk_jamontou_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_jamontou_dragao FOREIGN KEY (Nome_Dragao) REFERENCES Dragao(Nome)
+);
+
+CREATE TABLE Interpreta(
+    IDProfecia INT,
+    Codigo_Personagem INT,
+    Descricao TEXT,
+    Previsao INT,
+    Realizacao INT,
+
+    PRIMARY KEY (IDProfecia, Codigo_Personagem),
+    CONSTRAINT fk_interpreta_profecia FOREIGN KEY (IDProfecia) REFERENCES Profecia(Id_profecia),
+    CONSTRAINT fk_interpreta_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO)
+);
+
+
+CREATE TABLE Integra_se(
+    Codigo_Personagem INT,
+    Nome_Profissao VARCHAR(50),
+    ID_cidade INT,
+
+    PRIMARY KEY (Codigo_Personagem, Nome_Profissao, ID_cidade),
+    CONSTRAINT fk_integra_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Humanos(Codigo_Personagem),
+    CONSTRAINT fk_integra_profissao FOREIGN KEY (Nome_Profissao) REFERENCES Profissao(Nome),
+    CONSTRAINT fk_integra_cidade FOREIGN KEY (ID_cidade) REFERENCES Cidade(ID)
+);
+
+
+CREATE TABLE Pertencimento( --Posse de Armas
+    ID SERIAL PRIMARY KEY,
+    Periodo INT,
+    IDArma INT,
+    Codigo_Personagem INT,
+
+    CONSTRAINT fk_pertencimento_arma FOREIGN KEY (IDArma) REFERENCES Arma(ID),
+    CONSTRAINT fk_pertencimento_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO)
+);
+
+CREATE TABLE Participa_Profecia (
+    IDProfecia INT,
+    Codigo_Personagem INT,
+    Associacao VARCHAR(50),
+    DataInicio INT,
+    DataFim INT,
+
+    PRIMARY KEY (IDProfecia, Codigo_Personagem),
+    CONSTRAINT fk_participa_profecia FOREIGN KEY (IDProfecia) REFERENCES Profecia(Id_profecia),
+    CONSTRAINT fk_participa_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO)
+);
+
+CREATE TABLE Causam(
+    Codigo_Personagem INT,
+    ID_Morte INT,
+
+    PRIMARY KEY (Codigo_Personagem, ID_Morte),
+    CONSTRAINT fk_causam_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_causam_morte FOREIGN KEY (ID_Morte) REFERENCES Morte(ID)
+);
+
+CREATE TABLE ParticipouEM(
+    Codigo_Personagem INT,
+    ID_Batalha INT,
+    ID_Guerra INT,
+
+    PRIMARY KEY (Codigo_Personagem, ID_Batalha, ID_Guerra),
+    CONSTRAINT fk_part_em_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO),
+    CONSTRAINT fk_part_em_batalha FOREIGN KEY (ID_Batalha) REFERENCES Batalha(ID),
+    CONSTRAINT fk_part_em_guerra FOREIGN KEY (ID_Guerra) REFERENCES Guerra(ID)
+);
+
+CREATE TABLE MortoEm (
+    ID_Morte INT PRIMARY KEY,
+    ID_Batalha INT,
+    ID_Guerra INT,
+    
+    CONSTRAINT fk_mortoem_morte FOREIGN KEY (ID_Morte) REFERENCES Morte(ID),
+    CONSTRAINT fk_mortoem_batalha FOREIGN KEY (ID_Batalha) REFERENCES Batalha(ID),
+    CONSTRAINT fk_mortoem_guerra FOREIGN KEY (ID_Guerra) REFERENCES Guerra(ID)
+);
+
+CREATE TABLE FazParte_Casa (
+    NomeCasa VARCHAR(50),
+    Codigo_Personagem INT,
+    Data_Inicio INT,
+    Data_Fim INT,
+    
+    PRIMARY KEY (NomeCasa, Codigo_Personagem, Data_Inicio),
+    CONSTRAINT fk_fazparte_casa FOREIGN KEY (NomeCasa) REFERENCES Casa(Nome),
+    CONSTRAINT fk_fazparte_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO)
+);
+
+CREATE TABLE Fundou (
+    NomeCasa VARCHAR(50) PRIMARY KEY,
+    Codigo_Personagem INT,
+    
+    CONSTRAINT fk_fundou_casa FOREIGN KEY (NomeCasa) REFERENCES Casa(Nome),
+    CONSTRAINT fk_fundou_personagem FOREIGN KEY (Codigo_Personagem) REFERENCES Personagem(CODIGO)
+);
+
+CREATE TABLE Participou_Batalha (
+    ID_Batalha INT,
+    NomeCasa VARCHAR(50),
+    
+    PRIMARY KEY (ID_Batalha, NomeCasa),
+    CONSTRAINT fk_part_bat_batalha FOREIGN KEY (ID_Batalha) REFERENCES Batalha(ID),
+    CONSTRAINT fk_part_bat_casa FOREIGN KEY (NomeCasa) REFERENCES Casa(Nome)
+);
+
+CREATE TABLE Atua (
+    ID_Guerra INT,
+    NomeCasa VARCHAR(50),
+    
+    PRIMARY KEY (ID_Guerra, NomeCasa),
+    CONSTRAINT fk_atua_guerra FOREIGN KEY (ID_Guerra) REFERENCES Guerra(ID),
+    CONSTRAINT fk_atua_casa FOREIGN KEY (NomeCasa) REFERENCES Casa(Nome)
+);
+
+CREATE TABLE Vencedor (
+    ID_Batalha INT,
+    NomeCasa VARCHAR(50),
+    
+    PRIMARY KEY (ID_Batalha, NomeCasa),
+    CONSTRAINT fk_vencedor_batalha FOREIGN KEY (ID_Batalha) REFERENCES Batalha(ID),
+    CONSTRAINT fk_vencedor_casa FOREIGN KEY (NomeCasa) REFERENCES Casa(Nome)
+);
+
+CREATE TABLE Participa_Alianca (
+    Nome_Alianca VARCHAR(100),
+    NomeReino VARCHAR(50),
+    
+    PRIMARY KEY (Nome_Alianca, NomeReino),
+    CONSTRAINT fk_part_ali_alianca FOREIGN KEY (Nome_Alianca) REFERENCES Alianca(Nome),
+    CONSTRAINT fk_part_ali_reino FOREIGN KEY (NomeReino) REFERENCES Reino(Nome)
+);
+
+CREATE TABLE FazParteRC (
+    IDCidade INT,
+    NomeReino VARCHAR(50),
+    DataInicio INT,
+    DataFim INT,
+    
+    PRIMARY KEY (IDCidade, NomeReino, DataInicio),
+    CONSTRAINT fk_fazparterc_cidade FOREIGN KEY (IDCidade) REFERENCES Cidade(ID),
+    CONSTRAINT fk_fazparterc_reino FOREIGN KEY (NomeReino) REFERENCES Reino(Nome)
+);
